6e2c9b42a320ce77cc60f9252552e75e432abe2a
Arne Verleyen <ArneVerleyen@MacBook-Pro-van-Arne.local>
2020-10-18 19:30:38 +0200

    Push voor deadline

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+const { description } = require('../../package');
+
+module.exports = {
+    title: 'Tactile interfaces',
+    description: description,
+    head: [
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ],
+    
+
+}
